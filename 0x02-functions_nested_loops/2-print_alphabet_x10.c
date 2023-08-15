cfbe1e1e26f10af76289960be6c220b087eb2f4e
youssef <wissal2015.nassib@gmail.com>
2023-08-15 13:40:26 -0700

    l

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - function that print the alphabet 10 time in lower case
+ *
+ * Return: always 0
+ */
+void print_alphabet_x10(void)
+{
+	int i;
+	char j;
+
+	for (i = 0 ; i <= 9 ; i++)
+	{
+		for (j = 'a' ; j <= 'z' ; j++)
+			_putchar(j);
+	}
+	_putchar('\n');
+}
