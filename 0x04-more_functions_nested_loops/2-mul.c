00482716dbffef600291b32ecf5140dd7b1180f1
youssef <wissal2015.nassib@gmail.com>
2023-08-17 10:27:07 -0700

    l

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include "stdio.h"
+
+/**
+ * mul - function multipple two number
+ * @a: function parametter
+ * @b: function parameter
+ * Return: Always 0
+ */
+int mul(int a, int b)
+{
+	printf("the multiple of %d and %d is %d", a, b, a * b);
+	return (0);
+}
