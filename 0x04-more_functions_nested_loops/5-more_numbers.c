d831c99c3f0a9b7a54c33843517bebc00114cef6
youssef <wissal2015.nassib@gmail.com>
2023-08-17 11:41:00 -0700

    l

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * more_numbers - print 10times numbers
+ *
+ * Return: always succesed
+ */
+void more_numbers(void)
+{
+	int i;
+	int j;
+
+	for (j = 0 ; j <= 10 ; j++)
+	{
+		for (i = 0 ; i <= 14 ; i++)
+		{
+			_putchar(i + '0');
+		}
+		_putchar('\n');
+	}
+}
