c0b819b012294c23006e8a54292eac9075f8b236
root <wissal2015.nassib@gmail.com>
2024-01-16 06:49:46 -0800

    l

---
+++ 0x18-dynamic_libraries/all.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+/**
+ * add - add two numberse
+ * @a: first number
+ * @b: second number
+ * Return: addition
+ */
+int add(int a,int b)
+{
+	return (a + b);
+}
+/**
+ * sub - substract two integers
+ * @a: first number
+ * @b: second number
+ * Return: substraction
+ */
+int sub(int a,int b)
+{
+	return(a - b);
+}
+/**
+ * mod - modilation of two numbers
+ * @a: first number
+ * @b: second number
+ * Return: modilation of two numbers
+ */
+int mod(int a,int b)
+{
+	return(a % b);
+}
+/**
+ * div - division of two numbers
+ * @a: first number
+ * @b second number
+ * Return: division of two numbers
+ */
+int div(int a, int b)
+{
+	return(a / b);
+}
+/**
+ * mul - mulltiplaction of two numbers
+ * @a: first number
+ * @b: second number
+ * Return: multiplication of two numbers
+ */
+int mul(int a,int b)
+{
+	return(a * b);
+}
